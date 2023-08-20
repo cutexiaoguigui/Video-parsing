af2f9f8f911a4c309775098e712cf9b5e914658c
xiaoguigui <cutegui@cutexiaoguigui.eu.org>
2023-08-20 11:41:59 +0800

    Update data.js

--- static/js/data.js
+++ static/js/data.js
@@ -1,5 +1,6 @@
 const routes = [
-  {name: '主线', url: 'https://svip.bljiex.cc/?v='}, 
+  {name: '主线1', url: 'https://www.yemu.xyz/?url='}, 
+  {name: '主线2', url: 'https://svip.bljiex.cc/?v='}, 
   {name: '备用1', url: 'https://jx.xmflv.com/?url='},
   {name: '备用2', url: 'https://jx.jsonplayer.com/player/?url='},
   {name: '备用3', url: 'https://search.ymck.me/index.php?q='}, 
